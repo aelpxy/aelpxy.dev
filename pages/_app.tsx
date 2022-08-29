e1a7bb0734fe5b615afd88d405338ff14bc8476a
Aelpxy <aelpxy@protonmail.com>
2022-08-29 11:30:30 +0000

    feat: new features

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,15 +6,6 @@ import "../styles/main.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <link
-          rel="preload"
-          href="/fonts/Inter-roman.latin.var.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-      </Head>
       <Component {...pageProps} />
     </>
   );
--- theme.config.js
+++ theme.config.js
@@ -1,20 +1,56 @@
 const YEAR = new Date().getFullYear();
 
 export default {
+  head: ({ meta }) => {
+    return (
+      <>
+        <meta name="author" content="Aelpxy" />
+        <link rel="canonical" href="https://aelpxy.space" />
+        <meta name="title" content={meta.title} />
+        <meta property="description" content={meta.description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:url" content="https://aelpxy.space" />
+        {/* <meta
+          property="og:image"
+          content={meta.image || "https://aelpxy.space/logo.png"}
+        /> */}
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:site" content="@aelpxy" />
+        <meta property="twitter:title" content={meta.title} />
+        <meta property="twitter:description" content={meta.description} />
+        <meta property="twitter:url" content="https://aelpxy.space" />
+        {/* <meta
+          property="twitter:image"
+          content={meta.image || "https://aelpxy.space/logo.png"}
+        /> */}
+      </>
+    )
+  },
   footer: (
-    <footer>
-      <small>
+    <div>
+      <hr />
+      <a href="https://twitter.com/aelpxy" target="_blank">
+        Twitter
+      </a>
+      · {" "}
+      <a href="https://github.com/aelpxy" target="_blank">
+        GitHub
+      </a>
+      · {" "}
+      <a href="mailto:hello@aelpxy.space" target="_blank">
+      hello@aelpxy.space
+      </a>
+      <small style={{ display: "block", marginTop: "8rem" }}>
         <time>{YEAR}</time> © Aelpxy.
+        <style jsx>{`
+          a {
+            float: right;
+          }
+        `}</style>
       </small>
-      <style jsx>{`
-        footer {
-          margin-top: 8rem;
-        }
-        a {
-          float: right;
-        }
-      `}</style>
-    </footer>
+    </div>
   ),
   readMore: "Read More →",
   darkMode: true,
