5464c54f387556c9730fc03f0dac0892a518983d
Aelpxy <aelpxy@protonmail.com>
2024-05-10 17:36:56 +0600

    fix: dark mode issues

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -30,13 +30,13 @@ const Navbar = () => {
       animate={{ opacity: 1, y: 0 }}
       className='py-2 select-none sticky top-0 z-10'
     >
-      <div className='mx-auto max-w-7xl px-6 lg:px-32 py-2 select-none'>
-        <div className="relative z-[-1] flex place-items-center before:absolute before:h-[400px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-pink-300 before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-pink-200 after:via-pink-400 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-pink-700 before:dark:opacity-10 after:dark:from-pink-900 after:dark:via-[#dc38e4] after:dark:opacity-40 " />
+      <div className='relative z-[-5] flex place-items-center before:absolute before:h-[400px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-pink-950 before:to-transparent before:blur-2xl after:absolute after:-z-20 after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-pink-900 after:via-pink-900 after:blur-2xl' />
 
+      <div className='mx-auto max-w-7xl px-6 lg:px-32 py-2 select-none'>
         <motion.div
           initial={{ translateY: -20 }}
           animate={{ translateY: 0 }}
-          className='flex w-full items-center justify-between py-4 px-6 rounded-2xl bg-neutral-950 backdrop-filter bg-opacity-40 backdrop-blur-md border border-neutral-900'
+          className='flex w-full items-center justify-between py-4 px-6 rounded-2xl bg-neutral-950 backdrop-filter bg-opacity-40 backdrop-blur-md ' // border border-neutral-900
         >
           <div>
             <div className='flex lg:flex-1 select-none'>
